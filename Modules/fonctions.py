68987ded1116364c10d258a01c66ced07ec97279
iHelperRepo <86736499+iHelperRepo@users.noreply.github.com>
2021-10-07 19:17:24 +0200

    degre2 ajouté | optimisation degre1
    - gestion des erreurs du degre 1
    - création de la fonction de résolution du 2nd degré

--- Modules/fonctions.py
+++ Modules/fonctions.py
@@ -1,10 +1,12 @@
-import math
+from math import *
 #ax+b=c
 class degre1():
     def solve(equation):   
       try:
         if not "=" in equation:
-          raise ValueError
+          raise MemoryError
+        if not "x" in equation:
+          raise SyntaxError
         line = equation.replace(' ', "")
         line = line.replace('(', "")
         line = line.replace(')', "")
@@ -42,4 +44,35 @@ class degre1():
         return x
       except ValueError or TypeError or IndexError:
         return('Error - Equation must be ax+b=c, please check your entry')
-print(degre1.solve(input('Type your equation :')))
+      except SyntaxError:
+        return 'Syntaxe Error, x not found in equation'
+      except MemoryError:
+        return('Syntaxe Error, = not found, entry is not equation')
+
+class degre2():
+  def solve(a, b, c, complexMode=False): #défini la fonction avce les constantes et le mode complexe (désactivé par défaut)
+    try:
+      a= float(a) #
+      b=float(b)  # converti les constantes en nombre au cas ou
+      c=float(c)  #
+      delta = (b**2) - (4*a*c) # calcul de delta
+      if delta > 0: #cas supérieur à 0
+        x1 = (-b-sqrt(delta))/(2*a) #calcul des racines
+        x2 = (-b+sqrt(delta))/(2*a) # 
+        return [x1, x2] #renvoie les racines à la ligne d'appel
+      elif delta == 0: #cas égal 0
+        x = (-b)/(2*a) #calcul de la racine
+        return [x]
+      elif delta < 0 and complexMode: #delta negatif et solution complexes demandées
+        z1 = str((-b)/(2*a)) + '+i' + str(abs(sqrt(-delta)/2*a)) 
+        z2 = str((-b)/(2*a)) + '-i' + str(abs(sqrt(-delta)/2*a))
+        return [z1, z2]
+      else:
+        return None #delta négatif pas de solutio
+
+    except TypeError or ValueError:
+      return 'Error, invalid parameter' #gestion des erreurs relatives aux valeurs
+
+
+
+print(degre2.solve(input('a'), input('b'), input('c')))
