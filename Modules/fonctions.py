318f66987a32523bc40d7bb18d7fb572c1766fcd
iHelperRepo <86736499+iHelperRepo@users.noreply.github.com>
2021-10-09 16:55:20 +0200

    Mise à jour de fonctions.py
    - Commentaire du degré 1
    - meilleure gestion des erreurs (renvoie un vrai message d'erreur, cependant ne montre pas seulement la ligne d'appel dans le log)
    - correction de bugs dans la résolution du degré 2
    - ajout de la forme canonique du degré 2
    - meilleur formatage du résultat en integer pour le degré 1
    
    A FAIRE :
    - Ajouter forme factorisée degré 2
    - Formatage des résultats résultats du degré 2 comme pour le degré 1

--- Modules/fonctions.py
+++ Modules/fonctions.py
@@ -1,53 +1,46 @@
 from math import *
-#ax+b=c
+#équations de degré 1
 class degre1():
     def solve(equation):   
-      try:
-        if not "=" in equation:
-          raise MemoryError
-        if not "x" in equation:
-          raise SyntaxError
-        line = equation.replace(' ', "")
-        line = line.replace('(', "")
-        line = line.replace(')', "")
-        line = line.replace('+-', "-")
-        line = line = line.split("=")
-        c = float(line[1])
-        ab = line[0]
-        if "-" in ab:
-            operator = ab.find('-')
-            a = ab[:operator].replace('x',"")
-            if a != '':
-                a = float(a)
-            else:
-                return ('a = 0, No solutions')
-            ab = ab.split('-')
-            b = -1*(float(ab[1]))
-        elif "+" in ab:
-            operator = ab.find('+')
-            a = float(ab[:operator - 1].replace('x',""))
-            a = ab[:operator].replace('x',"")
-            if a != '':
-                a = float(a)
-            else:
-                return ('a = 0, No solutions')
-            ab = ab.split('+')
-            b = (float(ab[1]))
+      if not "=" in equation:                                                           #
+        raise SyntaxError("No '=' found in formula, equation degre 1 must be ax+b=c")   # verifie si l'entrée est une équation
+      if not "x" in equation:                                                           #
+        raise SyntaxError("No 'x' found in equation, equation degre 1 must be ax+b=c ") #
+      line = equation.replace(' ', "")  #
+      line = line.replace('(', "")      #
+      line = line.replace(')', "")      # formatage de l'équation sous forme ax+b=c sans espace/doubles opérateurs/parenthèses
+      line = line.replace('+-', "-")    #
+      line = line = line.split("=")   # sépare l'équation au niveau du = pour isoler c
+      c = float(line[1])      # stocke c sous forme de nombre   
+      ab = line[0]            # stacke ax+b sous forme de string 
+      if "-" in ab:     #si l'opération est une soustraction
+        operator = ab.find('-') #sépare ax et b au niveau du moins
+        a = ab[:operator].replace('x',"")   # récupère tous les caractères jusqu'à l'opérateur et enlève x pour isoler a 
+        if a != '': # vérifie que a ≠ 0
+          a = float(a) # si oui, le converti en nombre
         else:
-          a = float(ab.replace('x',''))
-          b = 0
-        x = (c-b)/a
-        x=str(x)
-        if x.endswith(".0"):
-          x = float(x)
-          x = round(x)
-        return x
-      except ValueError or TypeError or IndexError:
-        return('Error - Equation must be ax+b=c, please check your entry')
-      except SyntaxError:
-        return 'Syntaxe Error, x not found in equation'
-      except MemoryError:
-        return('Syntaxe Error, = not found, entry is not equation')
+          raise ValueError("Entry is not equation, a can't be 0") # sinon, renvoie une erreur
+        ab = ab.split('-') #sépare ax+b pour isoler b
+        b = -1*(float(ab[1])) #stocke b sous forme de nombre | * -1 car l'opérateur est '-'
+      elif "+" in ab: # meme chose si l'opérateur est positif (sauf que pas besoin de changer le signe de ||b||)
+        operator = ab.find('+')
+        a = float(ab[:operator - 1].replace('x',""))
+        a = ab[:operator].replace('x',"")
+        if a != '':
+          a = float(a)
+        else:
+          return ('a = 0, No solutions')
+        ab = ab.split('+')
+        b = (float(ab[1]))
+      else: # pas d'opérateur (b = 0)
+        a = float(ab.replace('x','')) # isole a sans x
+      b = 0
+      x = (c-b)/a
+      try:
+        int(x) # enlève le .0  si x est entier de façon plus conventionelle (;
+      except ValueError:
+        pass
+      return x
 
 class degre2():
   def solve(a, b, c, complexMode=False): #défini la fonction avce les constantes et le mode complexe (désactivé par défaut)
@@ -59,20 +52,41 @@ class degre2():
       if delta > 0: #cas supérieur à 0
         x1 = (-b-sqrt(delta))/(2*a) #calcul des racines
         x2 = (-b+sqrt(delta))/(2*a) # 
-        return [x1, x2] #renvoie les racines à la ligne d'appel
+        return [delta, x1, x2] #renvoie les racines à la ligne d'appel
       elif delta == 0: #cas égal 0
         x = (-b)/(2*a) #calcul de la racine
-        return [x]
+        return [delta, x]
       elif delta < 0 and complexMode: #delta negatif et solution complexes demandées
         z1 = str((-b)/(2*a)) + '+i' + str(abs(sqrt(-delta)/2*a)) 
         z2 = str((-b)/(2*a)) + '-i' + str(abs(sqrt(-delta)/2*a))
-        return [z1, z2]
+        return [delta, z1, z2]
       else:
-        return None #delta négatif pas de solutio
+        return [delta] #delta négatif pas de solution(s)
 
     except TypeError or ValueError:
-      return 'Error, invalid parameter' #gestion des erreurs relatives aux valeurs
+      raise ValueError('An error occured, please check your entries') #gestion des erreurs relatives aux valeurs
+
+  def canon(a, b, c):
+    roots = degre2.solve(a, b, c, False)
+    a= float(a) #
+    b=float(b)  # converti les constantes en nombre au cas ou
+    c=float(c)  #
+    beta = b/(2*a)
+    delta = roots[0]
+    if delta < 0:
+      raise ValueError('Delta is negative')
+    alpha = delta/(4*(a**2))
+    canonic = f"{a}[(x+{beta})-{alpha}]"
+    if a == 1:
+      canonic = canonic.replace(f"{a}[", "")
+      canonic = canonic.replace(f"]", "")
+    if alpha == 0:
+      canonic = canonic.replace(f"-{alpha}", "")
+    canonic = canonic.replace('+-', "-")
+    canonic = canonic.replace('--', "+")
+    return canonic
 
 
 
-print(degre2.solve(input('a'), input('b'), input('c')))
+print(degre2.canon(input('a'), input('b'), input('c')))
+
