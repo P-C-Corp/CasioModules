c15446327713ffbbcede4aee9a7f3a1bf028c66a
iHelperRepo <86736499+PetchouDev@users.noreply.github.com>
2021-10-17 20:54:52 +0200

    installer
    - ajout d'écrans à l'interface
    - début de code (problème de désactivation  de la checkbox)

--- installer/main.py
+++ installer/main.py
@@ -1,4 +1,39 @@
-from PyQt5 import *
+from PyQt5 import QtWidgets, uic
+import sys
+import os
+import platform
+import getpass
 
+def PathToFiles()->str:
+    if platform.system() == 'Windows':
+        path = str(r"c:\Users\{}\PCCorp".format(getpass.getuser()))
+        return path
+    else:
+        return f"/Users/{getpass.getuser()}/PCCorp"
 
-if __name__ == "__main__"
+path = os.path.abspath(os.path.dirname(__file__))
+
+class Connect(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Connect, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi(f'{path}/screens/ui/ConnexionScreen.ui', self) # Load the .ui file
+        cb = self.checkBox
+        cb.toggle()
+        try:
+            testFile = open(f'{PathToFiles}/CasioModules/ToDo.txt ')
+            testFile.close()
+            self.checkBox.setEnabled(False)
+        except FileNotFoundError :
+
+            cb.setEnabled(False)
+            cb.setText("Online mode required")
+            print(cb.text)
+        super(Connect, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi(f'{path}/screens/ui/ConnexionScreen.ui', self) # Load the .ui file
+        self.label_4.setText('<a href=\"https://github.com/P-C-Corp/CasioModules">See Documentation</a>')
+        self.show()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+    window = Connect() # Create an instance of our class
+    app.exec_() # Start the application
