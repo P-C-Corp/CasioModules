1f22753a35e6683e4243ac61fa49289d068d6015
Dhulmi <92446876+Dhulmi@users.noreply.github.com>
2021-10-21 23:32:28 +0200

    Complex en expert
    Début de création d'une liste avec plusieurs complexe pour réaliser les opération de base (chap 1)

---
+++ Modules/Complex.py
@@ -0,0 +1,64 @@
+from math import*
+
+#rules
+print('Enter 1 when you want to answer yes to the question')
+print('Enter 0 when you want to answer no to the question')
+
+class listeComplexe():
+    #pour utiliser ou quitter le programme
+    Origine = float(input('Wanna use the program? ='))
+    
+    if (Origine==True):
+        #definir la liste de complexes
+        Complexe = []
+        #valeur de i = sqrt(-1)
+        i = complex (0,1)
+        #pour définir le premier complexe
+        Complex = int(input("Wanna definit a complexe? ="))
+        #conditions
+        if (Complex==False):
+            #marque l'erreur
+            ##Chercher comment reboucler le programme
+            raise SyntaxError('must enter 1 for yes to definit it')
+        elif (Complex==True):
+            #premier complexe
+            DefinitLeComplexe = int(input('Just enter 1='))
+            #au cas où un idiot ne sait pas suivre des instructions
+            if (DefinitLeComplexe != 1):
+                raise SyntaxError('Told you faggot')
+            #définition de la partie réele t imaginaire du complexe
+            Rez = float(input('Rez='))
+            Imz = float(input('Imz='))
+            Complex1 = str((Rez+i*Imz))
+            #ajout du complexe à la liste
+            Complexe.append(Complex1)
+            #si ne veux pas d'autre complexe
+            c = int(input('Wanna other complexe? ='))
+            if (c==True):
+                #valeur pour arrêter le programme
+                b = int(input('Number of other complexe you want ='))
+                #boucle pour les autres complexes
+                while (DefinitLeComplexe==True):
+                    DefinitLeComplexe = int(input('Enter 1 ='))
+                    #si l'idiot ne sais toujours pas suivre une consigne
+                    if (DefinitLeComplexe != 1):
+                        raise SyntaxError('Told you faggot')
+                    # définition des parties réels et imaginaires des autres complexes
+                    Rez = float(input('Rez='))
+                    Imz = float(input('Imz='))
+                    Complex1 = str((Rez+i*Imz))
+                    #ajout à la liste de ses derniers
+                    Complexe.append(Complex1)
+                    #pour stopper la boucle au nombre de complexe voulu
+                    b -= 1
+                    #littéralement casser la boucle
+                    if (b==0):
+                        break
+                else:
+                    pass
+        #affiche la liste
+        print (Complexe)
+    
+    elif (Origine==False):
+        raise SyntaxError('must enter 1 for yes to use it')
+
