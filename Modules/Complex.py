ddd0f9914ed0c918204464c58298164fa0b68efe
Dhulmi <92446876+Dhulmi@users.noreply.github.com>
2021-10-22 15:40:24 +0200

    Complexe
    Retravaille de la class et suite

--- Modules/Complex.py
+++ Modules/Complex.py
@@ -6,76 +6,43 @@ print('Enter 0 when you want to answer no to the question')
 
 class Complexes():
     def Liste():
-        #pour utiliser ou quitter le programme
-        Origine = float(input('Wanna use the program? ='))
-        
-        if (Origine==True):
-            #definir la liste de complexes
-            Complexe = []
-            #valeur de i = sqrt(-1)
-            i = complex (0,1)
-            #pour définir le premier complexe
-            Complex = int(input("Wanna definit a complexe? ="))
-            #conditions
-            if (Complex==False):
-                #marque l'erreur
-                ##Chercher comment reboucler le programme
-                raise SyntaxError('must enter 1 for yes to definit it')
-            elif (Complex==True):
-                #premier complexe
-                DefinitLeComplexe = int(input('Just enter 1='))
-                #au cas où un idiot ne sait pas suivre des instructions
-                if (DefinitLeComplexe != 1):
-                    raise SyntaxError('Told you faggot')
-                #définition de la partie réele t imaginaire du complexe
-                Rez = float(input('Rez='))
-                Imz = float(input('Imz='))
-                Complex1 = f"{Rez}+j{Imz}"
-                #ajout du complexe à la liste
-                Complexe.append(Complex1)
-                #si ne veux pas d'autre complexe
-                c = int(input('Wanna other complexe? ='))
-                if (c==True):
-                    #valeur pour arrêter le programme
-                    b = int(input('Number of other complexe you want ='))
-                    #boucle pour les autres complexes
-                    while (DefinitLeComplexe==True):
-                        DefinitLeComplexe = int(input('Enter 1 ='))
-                        #si l'idiot ne sais toujours pas suivre une consigne
-                        if (DefinitLeComplexe != 1):
-                            raise SyntaxError('Told you faggot')
-                        # définition des parties réels et imaginaires des autres complexes
-                        Rez = float(input('Rez='))
-                        Imz = float(input('Imz='))
-                        Complex1 = f"{Rez}+j{Imz}"
-                        #ajout à la liste de ses derniers
-                        Complexe.append(Complex1)
-                        #pour stopper la boucle au nombre de complexe voulu
-                        b -= 1
-                        #littéralement casser la boucle
-                        if (b==0):
-                            break
-                    else:
-                        pass
-            #affiche la liste
-            return (Complexe)
-        
-        elif (Origine==False):
-            raise SyntaxError('must enter 1 for yes to use it')
-
+        Complexe = []
+        i = complex (0,1)
+        Complex = int(input("Wanna definit a complexe? ="))
+        if (Complex==True):
+            DefinitLeComplexe = int(input('Just enter 1='))
+            Rez = float(input('Rez='))
+            Imz = float(input('Imz='))
+            Complex1 = f"{Rez} + i {Imz}"
+            Complexe.append(Complex1)
+            c = int(input('Wanna other complexe? ='))
+            if (c==True):
+                b = int(input('Number of other complexe you want ='))
+                while (DefinitLeComplexe==True):
+                    DefinitLeComplexe = int(input('Enter 1 ='))
+                    Rez = float(input('Rez='))
+                    Imz = float(input('Imz='))
+                    Complex1 = f"{Rez} + i {Imz}"
+                    Complexe.append(Complex1)
+                    b -= 1
+                    if (b==0):
+                        break
+        return (Complexe)
 
 Liste = (Complexes.Liste())
 print(Liste)
 lenListe = len(Liste)
 rezs = {}
-imgs = {}
+imzs = {}
 for i in Liste:
     print (f"{Liste.index(i)} -> {i} ")
     a = i.split("+")
     rezs[i] = a[0]
-    imgs[i] = a[1].replace ("j","")
-#print (rezs)
-#print (imgs)
-wanted = Liste.pop(int(input('Nombre 1 = ?')))
-print (wanted)
-print(rezs[wanted])
+    imzs[i] = a[1].replace ("j","")
+wanted = []
+#pour le nb de complexe à sortir
+
+wantede = Liste.pop(int(input('Nombre 1 = ?')))
+print (wantede)
+print(rezs[wantede])
+print(imzs[wantede])
