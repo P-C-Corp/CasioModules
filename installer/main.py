e29a03cecd63fe454b7328f0c9fc9319c855f32b
iHelperRepo <86736499+PetchouDev@users.noreply.github.com>
2021-10-23 14:21:33 +0200

    mise à jour installer
    - adaptation pour le cross-platform (windows et unix)
    - correction de l'affichage du logo
    - structure du projet améliorées
    - écran de chargement opérationnel

--- installer/main.py
+++ installer/main.py
@@ -1,10 +1,11 @@
-from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
 
 from PyQt5.QtCore import QThread, pyqtSignal
 import sys
 import os
 import platform
 import getpass
+from PyQt5.QtGui import QPixmap
 import requests
 import time
 
@@ -14,16 +15,16 @@ app = None
 def isNetwork()->bool:
     try:
         request = requests.get('http://www.google.com', timeout=5)
-        print("Connected to the Internet")
+        return True
     except (requests.ConnectionError, requests.Timeout) as exception:
-        print("No internet connection.")
-
+        return False
 global path
 path = os.path.abspath(os.path.dirname(__file__))
 
 def PathToFiles()->str:
     if platform.system() == 'Windows':
         path = str(r"c:\Users\{}\PCCorp".format(getpass.getuser()))
+        
         return path
     else:
         return f"/Users/{getpass.getuser()}/PCCorp"
@@ -31,12 +32,15 @@ def PathToFiles()->str:
 def pathToUi()->str:
     global path
     if platform.system() == 'Windows':
-        thePath = path+"/screens/ui/windows"
+        thePath = path+r"\screens\ui\windows"
+        print()
+        
         return thePath
     else:
         return path+"/screens/ui/unix"
+    
 
-logo = "screens/images/logo.png"
+
 
 
 class Connect(QtWidgets.QMainWindow):
@@ -59,7 +63,8 @@ class Connect(QtWidgets.QMainWindow):
         
         self.pb = self.pushButton
         self.pb.clicked.connect(self.connectPushed)
-
+        
+        self.label_2.setPixmap(QtGui.QPixmap(f"{path}/screens/images/logo.png"))
         self.label_4.setText('<a href=\"https://github.com/P-C-Corp/CasioModules">See Documentation</a>')
         
         self.show()
@@ -76,9 +81,7 @@ class Connect(QtWidgets.QMainWindow):
         
         self.win.show()
         self.close()
-        for i in range(1, 101):
-            print(i)
-            Connexion.Progress(Connexion())
+        
         #event.accept()
 
 
@@ -89,21 +92,43 @@ class Connexion(QtWidgets.QMainWindow):
         
         super(Connexion, self).__init__() # Call the inherited classes __init__ method
         uic.loadUi(f'{pathToUi()}/connecting.ui', self) # Load the .ui file
-
+        self.label_2.setPixmap(QtGui.QPixmap(f"{path}/screens/images/logo.png"))
         self.progressBar.setValue(0)
 
-
+        self.status = bool
         self.show()
+        self.sendInvoice()
         
-    def Progress(self):
-        pass
+
+    def sendInvoice(self):
+        runnable = InvoiceRunnable(self.progressBar)
+        QtCore.QThreadPool.globalInstance().start(runnable)
     
     def closeWindow(self, event):
         self.window_closed.emit()
         event.accept()
         
+    
 
     
+
+class InvoiceRunnable(QtCore.QRunnable):
+    def __init__(self, progressbar):
+        QtCore.QRunnable.__init__(self)
+        self.progressbar = progressbar
+
+    def run(self):
+        for i in range(1, 101):
+            currentPercentage = i
+            time.sleep(0.02)
+            QtCore.QMetaObject.invokeMethod(self.progressbar, "setValue",
+            QtCore.Qt.QueuedConnection,
+            QtCore.Q_ARG(int, currentPercentage))
+            if currentPercentage == 40:
+                if not isNetwork():
+                    print("no connexion")
+                else:
+                    print("connected")
 
 
 if __name__ == "__main__":
