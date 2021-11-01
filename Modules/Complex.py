e2cb95af2ce4ef71654423a409b7d32e76e5295e
iHelperRepo <86736499+PetchouDev@users.noreply.github.com>
2021-11-01 17:14:33 +0100

    Complexes.py
    - correction du remplacement au lieu de l'ajout
    - explication du principe de dictionnaires  en commentaire

--- Modules/Complex.py
+++ Modules/Complex.py
@@ -44,12 +44,46 @@ for i in Liste:
 wanted = []
 d = int(input("Number of complexe needed ="))
 e = 0
-while (e==0):
-    wanted = Liste.pop(int(input('Nombre (1er de la liste est 0)= ')))
+while (e==0): # met while True: ça suffi et c'est plus clean
+    wanted.append(Liste.pop(int(input('Nombre (1er de la liste est 0)= '))))
     d -= 1
     if (d==0):
         break
 
 print (wanted)
-print(rezs[wanted])
-print(imzs[wanted])
+#print(rezs[wanted]) # aucun sens tu demande la partie réelle/imaginaire d'une liste de valeurs...
+#print(imzs[wanted]) 
+
+#un crée d'autres dicos et met tes valueurs en analysant ta liste au cas par cas :
+
+wanted_rezs = {} #
+wanted_imzs = {} # crée deux variables pour stocker les parties des nombres
+for i in wanted:
+    a = i.split("+") # sépare les parties
+    wanted_rezs[i] = a[0] # stocke la partie réelle
+    wanted_imzs[i] = a[1].replace ("j","") # stocke la partie imaginaire après avoir enlevé le "j" -> permet les opérations
+
+"""
+Principe du dico en python :
+mon_dico = {}
+le format est {clé1: val1, clé2: val2, etc.}
+
+les valeurs s'appellent avce leur clé comme pour une liste avec l'index
+exemple :
+print(mon_dico[clé1])
+>>>val1
+
+pour récupérer une partie d'un nombre :
+
+le format est {'complexe en forme str': la partie réelle oiu complexe selon le dico}
+
+exemple:
+Liste(Complexes.Liste())
+-> script pour séparer
+-> imprime les nombres et leur index
+-> crée le wanted et stocke les parties => du coup le premier tri sert à rien ?
+print(wanted_rezs['5+j6'])
+
+>>>5
+
+"""
