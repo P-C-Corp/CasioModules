c9062790f6d28666429d69c4d5fa92701b887514
Dhulmi <92446876+Dhulmi@users.noreply.github.com>
2021-11-02 19:30:43 +0100

    Update Complexe
    J'ai fait la soustraction. je ferai après la multiplication et division.
    J'ai fait de très léger changement sur le code initial et faudrait voir ce que j'ai mis entre """# """ à la fin et corriger si possible le problème avec explication sur comment faire.

--- Modules/Complex.py
+++ Modules/Complex.py
@@ -1,14 +1,17 @@
 from math import*
 
 #rules
-print('Enter 1 when you want to answer yes to the question')
 print('Enter 0 when you want to answer no to the question')
+print('Enter 1 when you want to answer yes to the question')
+print('Enter 2 for addition')
+print('Enter 3 for substraction')
+print('Enter 4 for multiplication')
+print('Enter 5 for division')
 
 class Complexes():
-    def Liste():
+    def liste():
         Complexe = []
         i = complex (0,1)
-        #Complex = int(input("Wanna definit a complexe? ="))
         Complex = True
         if (Complex==True):
             
@@ -17,7 +20,9 @@ class Complexes():
             Complex1 = f"{Rez}+i{Imz}"
             Complexe.append(Complex1)
             c = int(input('Wanna other complexe? ='))
+            
             if (c==True):
+                
                 b = int(input('Number of other complexe you want ='))
                 while (b > 0):
                     #DefinitLeComplexe = int(input('Enter 1 ='))
@@ -26,95 +31,61 @@ class Complexes():
                     Complex1 = f"{Rez}+i{Imz}"
                     Complexe.append(Complex1)
                     b -= 1
+
                     if (b==0):
                         break
                 
         return (Complexe)
 
-    def Addition():  #regarde pour bosser plus sous forme de fonctions dans la classe tu dois utiliser que un print en theorie en dehors
-        numbers = Complexes.Liste() # en gros tu géère et récupère tes nombres
+operation = int(input("operation ="))
+
+if (operation == 2):
+    def addition():
+        numbers = Complexes.liste() 
         rezs = {}
         imzs = {}
-        for i in numbers:  # tu sépare tes parties et tu les stocke sous forme de nombre pour les opérartions clé = position dans la liste
+        for i in numbers:
             a = i.split("+")
-            rezs[numbers.index(i)] = float(a[0])
-            imzs[numbers.index(i)] = float(a[1].replace ("i",""))
-        
+            rezs[numbers.index(i)] = float(a[0]) #explique pk a[0] d'où vient le 0 et le a ?
+            imzs[numbers.index(i)] = float(a[1].replace ("i","")) #explique pk a[1] d'où vient le 1 et le a ?
+            
         real = 0
         img = 0
-        for rez in rezs.keys(): #additionne toutes les parties réelles
+        for rez in rezs.keys():
             real = real + float(rezs[rez])
-        for imz in imzs.keys(): # additionne toutes les parties immaginaires
+        for imz in imzs.keys():
             img = img + float(imzs[imz])
-
-        
-        result = f"{real}+{img}i" # crée le résultat
-        result = result.replace('+-', '-') #corrige le signe si besoin
+            
+        result = f"{real}+{img}i"
+        if (img<0):
+            result = result.replace('+', '')
         return result
+    
+    print(addition())
 
-print(Complexes.Addition())
+if (operation == 3):
+    def substraction ():
+        numbers = Complexes.liste()
+        rezs = {}
+        imzs = {}
+        for i in numbers:
+            a = i.split("+")
+            rezs[numbers.index(i)] = float(a[0])
+            imzs[numbers.index(i)] = float(a[1].replace("i",""))
+        real = 0
+        img = 0
+        for rez in rezs.keys():
+            real = -real - float(rezs[rez])
+        for imz in imzs.keys():
+            img = -img - float(imzs[imz])
+            
+        result = f"{real}+{img}i"
+        if (img<0):
+            result = result.replace("+","")
+        return result
+    
+    print(substraction())
 
-""" # efface les 3 " au début et à la fin pour enlever le commentaire
-
-Liste = (Complexes.Liste())
-print(Liste)
-lenListe = len(Liste)
-rezs = {}
-imzs = {}
-for i in Liste:
-    print (f"{Liste.index(i)} -> {i} ")
-    a = i.split("+")
-    rezs[i] = a[0]
-    imzs[i] = a[1].replace ("i","")
-
-
-wanted = []
-d = int(input("Number of complexe needed ="))
-e = 0
-while (e==0): # met while True: ça suffi et c'est plus clean
-    wanted.append(Liste.pop(int(input('Nombre (1er de la liste est 0)= '))))
-    d -= 1
-    if (d==0):
-        break
-
-print (wanted)
-#print(rezs[wanted]) # aucun sens tu demande la partie réelle/imaginaire d'une liste de valeurs...
-#print(imzs[wanted]) 
-
-#un crée d'autres dicos et met tes valueurs en analysant ta liste au cas par cas :
-
-wanted_rezs = {} #
-wanted_imzs = {} # crée deux variables pour stocker les parties des nombres
-for i in wanted:
-    a = i.split("+") # sépare les parties
-    wanted_rezs[i] = a[0] # stocke la partie réelle
-    wanted_imzs[i] = a[1].replace ("i","") # stocke la partie imaginaire après avoir enlevé le "j" -> permet les opérations
-""" #efface pour enlever le commentaire
-
-
-
-
-"""
-Principe du dico en python :
-mon_dico = {}
-le format est {clé1: val1, clé2: val2, etc.}
-
-les valeurs s'appellent avce leur clé comme pour une liste avec l'index
-exemple :
-print(mon_dico[clé1])
->>>val1
-
-pour récupérer une partie d'un nombre :
-
-le format est {'complexe en forme str': la partie réelle oiu complexe selon le dico}
-
-exemple:
-Liste(Complexes.Liste())
--> script pour séparer
--> imprime les nombres et leur index
--> crée le wanted et stocke les parties => du coup le premier tri sert à rien ?
-print(wanted_rezs['5+j6'])
-
->>>5
-
-"""
+"""# GROS PROBLEME:
+le programme n'arrive pas à additiuonner et soustraire deux complexes identiques.
+Au lieu de les additionner et soustraires il renvoit juste l'un des deux :/"""
